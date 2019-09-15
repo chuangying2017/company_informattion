ca8801fa50f1fdbf0bf4f71dfc1fde5116fbdf55
chuangying2017 <631711487@qq.com>
2019-09-15 14:49:50 +0800

    创建数据插入

--- model.py
+++ model.py
@@ -1,13 +1,28 @@
 from table import Company, init_connect, operation_complete
 
 
-if __name__ == '__main__':
-    company = Company(company_name='长隆欢乐谷', phone='13059551109', email='631711487@qq.com')
+def create(**kwargs):
+
+    company = Company(
+        company_name=kwargs.get('company_name', ''),
+        phone=kwargs.get('phone', ''),
+        status_quo=kwargs.get('status_quo', ''),
+        trademark_info=kwargs.get('trademark_info', ''),
+        registered_capital=kwargs.get('registered_capital', ''),
+        date_of_establishment=kwargs.get('date_of_establishment', ''),
+        email=kwargs.get('email', ''),
+        address=kwargs.get('address', ''),
+        introduction=kwargs.get('introduction', ''),
+        register_address=kwargs.get('register_address', ''),
+        business_scope=kwargs.get('business_scope', ''),
+        industry=kwargs.get('industry', ''),
+        staff_size=kwargs.get('staff_size', ''),
+        former_name=kwargs.get('former_name', ''),
+        business_term=kwargs.get('business_term', ''),
+        date_of_approval=kwargs.get('date_of_approval', ''),
+        company_type=kwargs.get('company_type', '')
+    )
     session = init_connect()
-    data: list = []
-    for i in range(10):
-        data.append(Company(company_name='长隆欢乐谷', phone='13059551109', email='631711487@qq.com'))
-    print(data)
-    session.add_all(data)
+    session.add(company)
     operation_complete(session)
-    print('successfully operation complete')
+
