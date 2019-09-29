c972afc42142f1b8a1a209712a5a11c1f0a126f2
chuangying2017 <631711487@qq.com>
2019-09-29 17:55:44 +0800

    创建了日志信息 保存

--- qichacha.py
+++ qichacha.py
@@ -19,6 +19,7 @@ import math, re, shutil
 import openpyxl
 import xlrd
 import model
+import logging
 
 """
 企查查 数据查询 根据时间查询 每个小月查询100次
@@ -52,12 +53,33 @@ class AutomateChaCha:
         "Sec-Fetch-User": "?1"
     }
     record_prefix: str = 'already_search_word'
+    num_condition: int = 500
+    xls_storage_import: str = './xls/not_import/'
+    xls_storage_export: str = './xls/already_export/'
+    download_num: int = 10
+    each_hours: int = 1
+    hours_query_num: int = 100
+    second_num: int = 60
+    log_path: str = './log/'
+    log: object
 
     def __init__(self):
         self.session = requests.session()
         self.session.headers.update(self.headers)
+        if not os.path.exists(self.xls_storage_import):
+            os.makedirs(self.xls_storage_import)
+            os.makedirs(self.xls_storage_export)
+        self.init_log()
         # self.chrome_options = Options()
         # self.browser = webdriver.Chrome(options=self.chrome_options)
+
+    def init_log(self):
+        log_info = f'{time.strftime("%Y%m%d", time.localtime())}.log'
+        log_src = self.log_path + log_info
+        if not os.path.exists(self.log_path):
+            os.mkdir(self.log_path)
+
+        logging.basicConfig(filename=log_src, level=logging.DEBUG)
 
     def login(self):
         """
@@ -130,17 +152,33 @@ class AutomateChaCha:
             res_collect = res.status_code
             if res_collect == 200:
                 res_collect = res.text
-            return res_collect
         except requests.ConnectionError as r_err:
-            print(r_err)
+            self.init_log()
+            logging.error(r_err)
+            res_collect = 500
+        return res_collect
 
-    def download_excel(self):
-        pass
+    def parse_text(self, html: str = '', keyword: str = ''):
+        if html == '':
+            return '数据不能为空'
+        jq = PyQuery(html)
+        span_id = jq('#countOld').children('span').text()
+        total_page = int(span_id)
+        if total_page >= self.num_condition:
+            get_Excel_param = 'get_Excel_param'
+            self.session.get(self.home_url + get_Excel_param)
+            self.session.get(self.home_url + get_Excel_param, params={"key": keyword})
+            time.sleep(12)
+            res_collecl = self.session.get(self.home_url + 'user_download')
+            result = self.user_download(res_collecl.text) if res_collecl.status_code == 200 else res_collecl.status_code
+        else:
+            result = '搜索到的数据不足' + span_id
+        return result
 
     def data_import(self):
-        dir_path = r'E:\xls\not_import'
-        source_path = r'E:\xls\already_import'
-        res = []
+        dir_path = self.xls_storage_import
+        source_path = self.xls_storage_export
+        res_array = []
         ls_res = self.path_easy(dir_path)
         data: list = []
         ls_key = [
@@ -175,11 +213,15 @@ class AutomateChaCha:
             if result:
                 shutil.move(path, source_path)
                 data = []
-                res.append(result)
+                res_array.append(result)
 
-        print(res)
+        return res_array
 
     def path_easy(self, dir_path: str) -> list:
+        """
+
+        :rtype: object
+        """
         ls: list = []
         for main_dir, subdir, file_name_list in os.walk(dir_path):
             for file in file_name_list:
@@ -213,13 +255,38 @@ class AutomateChaCha:
         self.browser.get(url)
         print('success')
 
+    def user_download(self, text: str):
+        jq = PyQuery(text)
+        ptable = jq(".ptable").children().eq(1).children('td')('td:last-child')
+        download_link: str = ptable.children('a').attr('href')
+        download_link = download_link.replace('\\', '/')
+        res = requests.get(download_link, headers={
+            'Cookie': "QCCSESSID=4ntaav4ld45ba7n41p883i60s4; UM_distinctid=16d1fce710129b-03f99ca449408a-4c312373-2a3000-16d1fce71022ea; CNZZDATA1254842228=1899939659-1568191378-https%253A%252F%252Fsp0.baidu.com%252F%7C1569738004; zg_did=%7B%22did%22%3A%20%2216d1fce71461b8-0d12045cb17be4-4c312373-2a3000-16d1fce7147440%22%7D; zg_de1d1a35bfa24ce29bbf2c7eb17e6c4f=%7B%22sid%22%3A%201569739164221%2C%22updated%22%3A%201569739177830%2C%22info%22%3A%201569289112713%2C%22superProperty%22%3A%20%22%7B%7D%22%2C%22platform%22%3A%20%22%7B%7D%22%2C%22utm%22%3A%20%22%7B%7D%22%2C%22referrerDomain%22%3A%20%22%22%2C%22cuid%22%3A%20%222548f9ef0f2d55f89c36b786bb37af80%22%2C%22zs%22%3A%200%2C%22sc%22%3A%200%7D; Hm_lvt_3456bee468c83cc63fb5147f119f1075=1568797314,1569289113,1569295343,1569739165; _uab_collina=156819668675323725971416; acw_tc=b7f0d83715681966876446230e9b7dc05aea2176296c3d86eb375e30b7; Hm_lpvt_3456bee468c83cc63fb5147f119f1075=1569739178",
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+        })
+        if res.status_code == 200:
+            with open(self.xls_storage_import+time.strftime('%Y%m%d%H%M%S', time.localtime()) + '.xls', 'wb') as ws:
+                ws.write(res.content)
+            success = True
+        else:
+            success = res.status_code
+        return success
+
     def run(self):
-        self.search_data('东莞日')
+        pass
 
     """
     手动分析请求， 进行解析请求的方式！
     需要请求的url
     """
+
+    def http_(self, path: str = 'user_download'):
+        res = self.session.get(self.home_url + path)
+        if res.status_code == 200:
+            text = res.text
+        else:
+            text = res.status_code
+        return text
 
     def operation_manual(self):
         pass
@@ -229,7 +296,6 @@ class AutomateChaCha:
         获取 中文词组库
         :return:
         """
-        city_list: list = []
         if not os.path.exists('zuci.txt'):
             with open('city.txt', 'r') as citys, open('hanzi1.txt', 'r') as hanzi1, open('zuci.txt', 'w') as zuci:
                 city = citys.read().split(',')
@@ -241,21 +307,21 @@ class AutomateChaCha:
                         city_ls.append(cs + word)
                     zuci_library[cs] = city_ls
                 zuci.write(json.dumps(zuci_library))
-                print(zuci_library.keys())
-                print('\n')
-                print(time.strftime('%H:%M:%S', time.localtime()))
+                # print(zuci_library.keys())
+                # print('\n')
+                # print(time.strftime('%H:%M:%S', time.localtime()))
         else:
             with open('zuci.txt') as zuci:
-                hanzi_zuci: dict = json.loads(zuci.read())
+                zuci_library: dict = json.loads(zuci.read())
 
-            if city_name is '':
-                city_list: list = hanzi_zuci.get(list(hanzi_zuci.keys())[0])
-            else:
-                city_list: list = hanzi_zuci.get(city_name, [])
+        city_name = list(zuci_library.keys())[0] if city_name is '' else city_name
+
+        city_list: list = zuci_library.get(city_name, [])
 
         return city_list
 
 
 if __name__ == '__main__':
     chacha = AutomateChaCha()
-    print(chacha.run())
+    chacha.run()
+
